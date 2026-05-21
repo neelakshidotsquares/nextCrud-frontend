814f109c8278d03222e85000d90c8189d9f9d8b4
neelakshidotsquares <neelakshi.sahu@dotsquares.com>
2026-05-21 19:28:00 +0530

    update the ui

--- app/register/page.js
+++ app/register/page.js
@@ -14,6 +14,7 @@ import { extractApiError } from "@/utils/apiError";
 
 const initialState = { name: "", email: "", address: "", password: "", confirmPassword: "" };
 
+
 export default function RegisterPage() {
   useGuestRoute();
   const { register } = useAuth();
